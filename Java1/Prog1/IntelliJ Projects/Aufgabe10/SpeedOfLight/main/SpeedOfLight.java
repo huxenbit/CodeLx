918c28269c9316a48276f3a406d27c756048a1d4
huxenbit <gianluca.scuzzarella@gmail.com>
2026-01-20 21:23:01 +0100

    changes

--- Java1/Prog1/IntelliJ Projects/Aufgabe10/SpeedOfLight/main/SpeedOfLight.java
+++ Java1/Prog1/IntelliJ Projects/Aufgabe10/SpeedOfLight/main/SpeedOfLight.java
@@ -1,9 +1,15 @@
 package main;
 
+import java.util.Locale;
+
 public class SpeedOfLight {
 	public static final long SPEED = 299_792_458; // m pro sek
 
 	public static double fraction(double factor) {
 		return ((double) SPEED) * factor;
+	}
+
+	public static void printFraction(double factor) {
+		System.out.format(Locale.GERMANY, "Multipliziert mit %.3f: %,d m/s\n", factor, (long) fraction(factor));
 	}
 }
--- Java1/Prog1/IntelliJ Projects/Aufgabe10/SpeedOfLight/test/SpeedOfLightTestDrive.java
+++ Java1/Prog1/IntelliJ Projects/Aufgabe10/SpeedOfLight/test/SpeedOfLightTestDrive.java
@@ -18,6 +18,24 @@ public class SpeedOfLightTestDrive {
 			randomNum = 1.0;
 		}
 
-		System.out.format(Locale.GERMAN, "Multipliziert mit %.3f: %,d m/s \n", randomNum, (long) SpeedOfLight.fraction(randomNum));
+		SpeedOfLight.printFraction(randomNum);
+///////////////////////////////////////////////////////
+		randomNum = Math.random();
+
+		//inkludiere 1 wenn randomNum sehr nah dran ist
+		if (randomNum >= 0.9999) {
+			randomNum = 1.0;
+		}
+
+		SpeedOfLight.printFraction(randomNum);
+//////////////////////////////////////////////////////
+		randomNum = Math.random();
+
+		//inkludiere 1 wenn randomNum sehr nah dran ist
+		if (randomNum >= 0.9999) {
+			randomNum = 1.0;
+		}
+
+		SpeedOfLight.printFraction(randomNum);
 	} 
 }
